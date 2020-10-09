7793c79bbb282c5b2dce1ce5d4bfa8e0ff74d26e
l0xy <l0xy@pm.me>
2020-10-09 16:35:20 -0500

    finished bash completion ($BASH_COMP) and man page ($MAN) functionality

--- provii.sh
+++ provii.sh
@@ -35,6 +35,20 @@ if command -v tput >/dev/null 2>&1; then
   fi
 fi
 
+
+create_dir() {
+	if [ ! -d "$1" ] && [ -w "$(dirname "$1")" ]; then
+			mkdir -p "$1"
+			if [ "$?" -eq 0 ]; then
+				log --created "$1"
+			else
+				warn Failed to create "$1"
+				return 1
+			fi
+	fi
+}
+export -f create_dir
+
 ls_installers() {
 	GITHUB_QUERY_LS="$( printf \
 		"https://api.github.com/repos/l0xy/provii/contents/installs%s" \
@@ -250,6 +264,7 @@ warn() {
 
 err() {
   echo "err:" "$@"
+	exit 1
 }
 
 fn_warn=$(
@@ -281,7 +296,8 @@ run_installer() {
     PROVII_CACHE=/tmp
   fi
   PROVII_LOG=$PROVII_CACHE/run.log
-
+	PROVII_OS="$(uname -s)"
+	PROVII_ARCH="$(uname -m)"
   set -a
 
   if [ "$(id -u)" -eq "0" ]; then
@@ -295,34 +311,73 @@ run_installer() {
     PV_DATA=${SYS_DATA-/usr/local/share}
     PV_CFG=${SYS_CFG-/etc}
     PV_SYSD=${SYS_SYSD-/etc/systemd/system}
-
-    for manpagepath in $(manpath -g | tr : $'\n'); do
-      if [ -w "$manpagepath" ]; then
-        PV_MAN="$manpagepath"
-        break
-      fi
-      "$PV_MAN:+$(warn 'no writable path for man pages found; man pages will not be installed')"
-    done
-
     PV_BASH_COMP=${SYS_BASH_COMP-/etc/bash_completion.d}
+		if command -v manpath &>/dev/null; then
+			for MAN_PATH in $(manpath -g | tr : $'\n'); do
+				if [ -d "$MAN_PATH" ] && [ -w "$MAN_PATH" ]; then
+					PV_MAN="$MAN_PATH"
+					break
+				fi
+			done
+		fi
     if command -v zsh >/dev/null 2>&1; then
       PV_ZSH_COMP=${SYS_ZSH_COMP-/usr/local/share/zsh/vendor-completions}
     fi
+
   elif [ "$PV_SCOPE" == user ]; then
     PV_BIN=${USER_BIN-$HOME/.local/bin}
     PV_CFG=${USER_CFG-$HOME/.config}
     PV_DATA=${USER_DATA-$HOME/.local/share}
     PV_SYSD=${USER_SYSD-$HOME/.config/systemd/user.control}
-    PV_MAN=${USER_MAN-"$(manpath | tr : $'\n' | grep -m 1 "$HOME")"}
+		if command -v manpath &>/dev/null; then
+			PV_MAN=${USER_MAN-"$(manpath | tr : $'\n' | grep -m 1 "$HOME")"}
+		fi
+		
     if [ -z "$PV_MAN" ]; then
-      mkdir -p $PV_DATA/man/man{1,8}
-      echo "$PV_DATA/man" >$HOME/.manpath
+			PV_MAN="$PV_DATA/man"
+      mkdir -p "$PV_MAN/man{1,8}"
+      echo "$PV_MAN" > "$HOME/.manpath"
     fi
-    if [ -n "$XDG_CONFIG_HOME" ]; then
-      PV_BASH_COMP=${USER_BASH_COMP-$XDG_CONFIG_HOME/bash_completion}
-    else
-      PV_BASH_COMP=${USER_BASH_COMP-$HOME/.bash_completion}
-    fi
+
+	# bash-completion 2.9 introduced ${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion.d
+	if command -v rpm &>/dev/null; then
+		QUERYPKG_CMD_FMT='rpm -qa --queryformat %s  name=%s'
+	elif command -v dpkg-query &>/dev/null; then
+		QUERYPKG_CMD_FMT='dpkg-query --showformat=%s --show %s'
+	fi
+
+	if [ "$QUERYPKG_CMD_FMT" ]; then
+		BASH_COMP_VER="$($(printf "$QUERYPKG_CMD_FMT" '%{VERSION}\n' 'bash-completion' ))"
+	else
+		warn "Could not locate either rpm / dkpg-query command to determine version of bash-completion"
+	fi
+
+	if [ -n "$BASH_COMP_VER" ]; then
+		XDG_ENABLED_BASH_COMP_VER='2.9'
+		if [ "${BASH_COMP_VER//./}" -ge "${XDG_ENABLED_BASH_COMP_VER//./}" ] \
+		&& [ -n "$XDG_DATA_HOME" ] && [ -n "$XDG_CONFIG_HOME" ]; then
+			PV_BASH_COMP="${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion.d"
+			PV_BASH_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion" \
+				&& create_dir "$XDG_CONFIG_HOME"
+		else
+			PV_BASH_COMP="$HOME/.bash-completion.d"
+			PV_BASH_CONFIG="$HOME/.bash_completion"
+		fi
+		create_dir "$PV_BASH_COMP"
+
+		# make sure there's a line in bash_completion (config) to
+		# source the files in bash-completion.d gets sourced
+		COMP_DIR_RE="$( printf '^[^#].*%s' "${PV_BASH_COMP//\./\\.}" )"
+		if ! grep -q "$COMP_DIR_RE" "$PV_BASH_CONFIG" 2>/dev/null; then
+			printf '
+for i in %s/*; do
+	if [ -r "$i" ]; then
+		. "$i"
+	fi
+done' "$PV_BASH_COMP" >> "$PV_BASH_CONFIG"
+		fi
+	fi
+
     if command -v zsh >/dev/null 2>&1; then
       PV_ZSH_COMP=${USER_ZSH_COMP-$ZSH_CUSTOM}
     fi
@@ -359,7 +414,6 @@ run_installer() {
   SCRIPTLET="$(curl -sSL $GITHUB_QUERY |
     jq '.download_url' |
     xargs curl -sSL)"
-  echo "$SCRIPTLET"
 
   /usr/bin/env - \
     PROVII_LOG="$PROVII_LOG" \
@@ -368,8 +422,8 @@ run_installer() {
     CFG=$PV_CFG \
     SYSD=$PV_SYSD \
     MAN="$PV_MAN" \
-    BASH_COMPLETION=$PV_BASH_COMP \
-    ZSH_COMPLETION=$PV_ZSH_COMP \
+    BASH_COMP=$PV_BASH_COMP \
+    ZSH_COMP=$PV_ZSH_COMP \
     INSTALLER=$INSTALLER \
     "$fn_github" \
     "$fn___dl_github_asset" \
